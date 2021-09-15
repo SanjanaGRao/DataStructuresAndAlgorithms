aff4e5cea1606bfa53d2e77fe5971af92f810c7f
Sanjana Rao <sanjanagrao99@gmail.com>
2021-09-15 16:34:19 +0530

    Modified: Insertion Sort of Strings

--- src/main/java/com/bridgeLabz/DataStructuresAndAlgorithms/InsertionSort.java
+++ src/main/java/com/bridgeLabz/DataStructuresAndAlgorithms/InsertionSort.java
@@ -2,11 +2,21 @@ package com.bridgeLabz.DataStructuresAndAlgorithms;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.util.Arrays;
 import java.util.Scanner;
-
+/*
+ * The class InsertionSort sorts the list of strings
+ * @author Sanjana Rao
+ * @since 13-09-2021
+ */
 public class InsertionSort {
 
+	/*
+	 * the class insertionSort takes in the parameter stringArray which reads the elements from the file
+	 * @param n is used to find the length of the array
+	 * @param key is used to compare the order of the array
+	 * for loop is used to traverse through the list and sort in ascending order.
+	 * Second for loop is used to display the elements after sorting.
+	 */
 	private static void insertionSort(String[] stringArray) 
 	{
 		int n = stringArray.length;
@@ -26,7 +36,12 @@ public class InsertionSort {
             System.out.print(stringArray[i]+" ");
         }		
 	}
-
+	
+	/*
+	 * The main function has an object of BufferedReader and Scanner class to read the data from the file
+	 * The words in a file are comma separated and stored in an array.
+	 * The function insertionSort is performed on that array.
+	 */
 	public static void main(String[] args) throws Exception
 	{
 		BufferedReader buffereader = new BufferedReader(new FileReader("C:\\Users\\sanjana.rao_ymediala\\eclipse-workspace\\DataStructuresAndAlgorithms\\src\\main\\java\\com\\bridgeLabz\\DataStructuresAndAlgorithms\\binary.txt"));
@@ -35,7 +50,6 @@ public class InsertionSort {
         while ((line = buffereader.readLine()) != null) 
         {
           String[] splitArray = line.split(",");
-          Arrays.sort(splitArray);
           insertionSort(splitArray);
         }
         sc.close();
